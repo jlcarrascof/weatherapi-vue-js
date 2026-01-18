2f47d75f25b62630f0ef18d4d6b3acba1f8d1271
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-17 22:51:32 -0400

    feat: add country data to Formulario component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,5 +1,15 @@
 <script setup>
 
+    const paises = [
+        { codigo: 'US', nombre: 'Estados Unidos' },
+        { codigo: 'MX', nombre: 'México' },
+        { codigo: 'AR', nombre: 'Argentina' },
+        { codigo: 'CO', nombre: 'Colombia' },
+        { codigo: 'CR', nombre: 'Costa Rica' },
+        { codigo: 'ES', nombre: 'España' },
+        { codigo: 'PE', nombre: 'Perú' }
+    ]
+
 </script>
 
 <template>
