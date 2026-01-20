d62bf6ca45a65d182a98731a7833b343384908b6
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-20 11:49:55 -0400

    feat: define emit for obtener-clima event in Formulario component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,6 +10,8 @@
 
     const error = ref('')
 
+    const emit = defineEmits(['obtener-clima'])
+
     const paises = [
         { codigo: 'US', nombre: 'Estados Unidos' },
         { codigo: 'MX', nombre: 'México' },
@@ -27,6 +29,7 @@
         }
 
         error.value = ''
+        emit('obtener-clima')
     }
 
 </script>
