fff9aa982d5d1061bbf35095a974a6826c58b9dd
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-24 20:27:35 -0400

    feat: add console log for URL in obtenerClima function

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -7,6 +7,8 @@ export default function useClima() {
         const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},
                     ${pais}&limit=1&appid=${key}`;
 
+        console.log(url);            
+
         // Get the weather
 
 
