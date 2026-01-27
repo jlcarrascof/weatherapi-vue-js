00d5b1ac238bf03045160bc5ac1afbbd13923ad4
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-26 23:01:15 -0400

    feat: include clima state in useClima function for improved weather data handling

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
     import Formulario from './components/Formulario.vue';
     import useClima from './composables/useClima';
 
-    const { obtenerClima } = useClima(); 
+    const { obtenerClima, clima } = useClima(); 
 
-    // console.log(import.meta.env);
 </script>
 
 <template>
