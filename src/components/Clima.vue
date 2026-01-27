1627e63892fddc4df50145616e44389ce7a364de
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-26 23:05:40 -0400

    feat: update Clima component template to display weather information

--- src/components/Clima.vue
+++ src/components/Clima.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
-    <div>
-
+    <div class="resultado">
+        <h2>Weather in: </h2>
+        <p class="actual"></p>
+        <div class="temperaturas">
+            <p>Min: <span>&deg;C</span></p>
+            <p>Max: <span>&deg;C</span></p>
+        </div>
     </div>
 </template>
 
