3f77f18fef2fdd20c6685456f830a38359ade90e
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-28 18:34:20 -0400

    feat: update Clima component to display min and max temperatures

--- src/components/Clima.vue
+++ src/components/Clima.vue
@@ -16,8 +16,8 @@
         <h2>Weather in: {{ clima.name }}</h2>
         <p class="actual">{{ formatearTemperatura(clima.main.temp) }}&deg;C</p>
         <div class="temperaturas">
-            <p>Min: <span>&deg;C</span></p>
-            <p>Max: <span>&deg;C</span></p>
+            <p>Min: <span>{{ formatearTemperatura(clima.main.temp_min) }}&deg;C</span></p>
+            <p>Max: <span>{{ formatearTemperatura(clima.main.temp_max) }}&deg;C</span></p>
         </div>
     </div>
 </template>
